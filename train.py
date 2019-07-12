531196a5d85687f01e1c1b41718eb2c7fce90311
HViktorTsoi <cuijiahetony@live.com>
2019-07-12 15:12:05 +0800

    finetune on kitti implemented. validation code refactored.

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ import numpy as np
 from dataloader.data_loader import DataLoaderSceneFlow, DataLoaderKITTI
 
 
-def train(ckpt_path, train_data_loader, val_data_loader):
+def train(ckpt_path, train_data_loader, val_data_loader, save_model_name):
     """
     训练
     :param val_data_loader:
@@ -56,13 +56,13 @@ def train(ckpt_path, train_data_loader, val_data_loader):
             print('EPOCH LOSS: {}'.format(epoch_loss / step))
 
             # 验证
-            val(sess, model, val_data_loader, vis=False)
+            val(sess, model, val_data_loader, vis=False, save_fig=False)
 
             # 保存模型
-            saver.save(sess, save_path='./ckpt/scene_flow_hgls.ckpt', global_step=epoch)
+            saver.save(sess, save_path='./ckpt/{}.ckpt'.format(save_model_name), global_step=epoch)
 
 
-def val(sess, model, data_loader, vis=False):
+def val(sess, model: PSMNet, data_loader, vis=False, save_fig=False):
     """
     验证
     :param sess:
@@ -78,16 +78,19 @@ def val(sess, model, data_loader, vis=False):
             left_imgs=imgL_crop,
             right_imgs=imgR_crop,
         )
-        # 计算误差
-        error_npx = utils.compute_npx_error(prediction, groundtruth, n=5)
-        error_total.append(error_npx)
-        print('npx-Error: {}'.format(error_npx))
+
+        if groundtruth is not None:
+            # 计算误差
+            error_npx = utils.compute_npx_error(prediction, groundtruth, n=5)
+            error_total.append(error_npx)
+            print('npx-Error: {}'.format(error_npx))
 
         # 可视化
-        if vis:
-            for img_id in range(len(prediction)):
+        for img_id in range(len(prediction)):
+            if save_fig:
                 cv2.imwrite('./vis/{:05d}_{:03d}.png'.format(step, img_id), prediction[img_id])
 
+            if vis:
                 plt.gcf().set_size_inches(10, 4)
                 plt.subplot(1, 2, 1)
                 plt.imshow(imgL_crop[img_id] * 0.25 + 0.4)
@@ -100,31 +103,28 @@ def val(sess, model, data_loader, vis=False):
     return np.average(error_total)
 
 
-def tests(ckpt_path, vis=False):
+def finetune():
     """
-    测试
-    :param ckpt_path:
-    :param vis:
+    在KITTI数据集上调优
     :return:
     """
-    with tf.Session() as sess:
-        # 构建模型
-        model = PSMNet(width=config.TRAIN_CROP_WIDTH, height=config.TRAIN_CROP_HEIGHT, channels=config.IMG_N_CHANNEL,
-                       head_type=config.HEAD_STACKED_HOURGLASS, batch_size=config.VAL_BATCH_SIZE)
-        model.build_net()
-
-        saver = tf.train.Saver()
-        saver.restore(sess, save_path=ckpt_path)
-
-        val(sess, model, vis=vis)
+    train(
+        ckpt_path='./ckpt/scene_flow_hgls.ckpt-7',
+        train_data_loader=DataLoaderKITTI(batch_size=config.TRAIN_BATCH_SIZE, max_disp=config.MAX_DISP),
+        val_data_loader=DataLoaderKITTI(batch_size=config.VAL_BATCH_SIZE, max_disp=config.MAX_DISP),
+        save_model_name='KITTI_hgls'
+    )
 
 
 if __name__ == '__main__':
     # os.environ['CUDA_VISIBLE_DEVICES'] = '1'
     # tests(ckpt_path='./ckpt/scene_flow_hgls.ckpt-32', vis=True)
-    train(
-        ckpt_path='./ckpt/scene_flow_hgls.ckpt-6',
-        # 载入数据集
-        train_data_loader=DataLoaderSceneFlow(batch_size=config.TRAIN_BATCH_SIZE, max_disp=config.MAX_DISP),
-        val_data_loader=DataLoaderSceneFlow(batch_size=config.VAL_BATCH_SIZE, max_disp=config.MAX_DISP),
-    )
+    # train(
+    #     ckpt_path='./ckpt/scene_flow_hgls.ckpt-6',
+    #     # 载入数据集
+    #     train_data_loader=DataLoaderSceneFlow(batch_size=config.TRAIN_BATCH_SIZE, max_disp=config.MAX_DISP),
+    #     val_data_loader=DataLoaderSceneFlow(batch_size=config.VAL_BATCH_SIZE, max_disp=config.MAX_DISP),
+    #     save_model_name='scene_flow_hgls'
+    # )
+    # 调优
+    finetune()
